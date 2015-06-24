4f4329a039c0570a0f56c57d466ea62e5c879049
Shafqat Dulal <shafqat.dulal@gmail.com>
2015-06-24 14:45:02 -0700

    Add links to some experiences

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,22 +6,26 @@ app.controller('MainController', ['$scope', function($scope) {
         {
             title: 'Volunteer Versus',
             text: '"Which group volunteered the most?" "Who has the most hours?" These questions have come up often over the years in my experiences volunteering. Inspired by a strong rivalry between two famous volunteering organizations as well as the inconveniences of viewing volunteering records, I sought out to design an app that would simplify managing records as well as bring stats-based competition into volunteering. The result is Volunteer Versus! Doing the project was very fun as I really got to use Ruby on Rails as well as multiple other related tools. Worked across the stack; did this solo!',
-            image: 'assets/img/vv.png'
+            image: 'assets/img/vv.png',
+            link: 'https://volunteerversus.herokuapp.com'
         },
         {
             title: 'Gitlet',
             text: 'I made a copy of Git in Java. Yes, Git—the exact version control system that was involved in pushing this very website! Although Gitlet was given as a class project, I had to design everything from scratch—no skeleton code or anything like that. Another thing was that I had to write all the tests by myself. All these things were definitely non-trivial and the project was relatively large. Certainly, this was one of the most practical and challenging projects I have done to date.',
-            image: 'assets/img/gitlet.png'
+            image: 'assets/img/gitlet.png',
+            link: '#'
         },
         {
             title: 'Upsilon Pi Epsilon',
             text: 'I contributed to the web development committee at UPE, the Computer Science Honor Society at UC Berkeley. Me and a few others worked on adding new features to their website and improving the code base. Recently, we improved office hours such that officers could manage hours with a simple interface—no code knowledge needed. Also, we added the ability to schedule appointments for mock interviews and other things. It has been a great learning experience; I picked up some knowledge of the Django framework (and ultimately backend) here!',
-            image: 'assets/img/upe.png'
+            image: 'assets/img/upe.png',
+            link: 'http://upe.berkeley.edu'
         },
         {
             title: 'Oppia',
             text: 'Founded by a few employees at Google, Oppia is an open-source project that aims to allow anyone to create interactive "explorations" and simulate one-on-one tutoring. I contributed to the project by adding infinite scrolling to the gallery on the website, picking up AngularJS in the process! Being only in the first semester at UC Berkeley at the time, I thought that this project was great and because of it, I was able to start contributing using my programming skills early on!',
-            image: 'assets/img/oppia.png'
+            image: 'assets/img/oppia.png',
+            link: 'https://www.oppia.org'
         },
     ];
     $scope.interests =
